a06783097b7bbacea168b81af5d0e189949eb924
Matias Garcia <mat.ant.gar@gmail.com>
2026-05-14 14:52:27 -0500

    removing build stuff

--- Networking/client_testing.cpp
+++ Networking/client_testing.cpp
@@ -1,8 +1,10 @@
 #include "Networking.hpp"
 #include <string>
 #include <iostream>
-
-int main() {
+/**
+ * takes argument from command line for message text to send to server, defaults to "Hello, Server!" if no argument is provided
+ */
+int main(int argc, char *argv[]) {
     // TCP_Client client;
     TCP_Client client(8080, INADDR_LOOPBACK); // Connect to localhost on port 8080
     std::cout << "Client initialized." << std::endl;
@@ -12,8 +14,12 @@ int main() {
     std::cout << "Connected to server." << std::endl;
 
     std::cout << "Sending message to server..." << std::endl;
-    client.write_to_server("Hello, Server!");
-
+    //get all subsiquent arguments from argv and concatenate them into a single message
+    std::string message = (argc > 1) ? std::string(argv[1]) : "Hello, Server!";
+    for (int i = 2; i < argc; i++) {
+        message += " " + std::string(argv[i]);
+    }
+    client.write_to_server(message);
     std::cout << "Reading response from server..." << std::endl;
     std::string response = client.read_from_server();
     std::cout << "Received from server: " << response << std::endl;
